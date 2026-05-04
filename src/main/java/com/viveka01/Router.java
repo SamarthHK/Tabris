4bedf431088f3c81d3bbaea8b818ec566283b14a
Viveka-01 <SamarthHKPersonal@outlook.com>
2026-05-03 18:09:30 -0700

    Save before moving front end folder

--- src/main/java/com/viveka01/Router.java
+++ src/main/java/com/viveka01/Router.java
@@ -13,8 +13,15 @@ import java.lang.NullPointerException;
 public class Router {
     private static Dictionary<String,Dictionary<Method,String>> route = new Hashtable<>();
     static{
-        addRoute("/",Method.GET,"src/main/frontEnd/Remixxer-Art-Demo-Front-End/mainRemixxerPage.html");
-        addRoute("/favicon.ico",Method.GET,"src\\main\\frontEnd\\Remixxer-Art-Demo-Front-End\\kaoru.png");
+        
+        addRoute("/",Method.GET,"src\\main\\frontEnd\\premain.html");
+        addRoute("/main.html",Method.GET,"src\\main\\frontEnd\\main.html");
+        addRoute("/externalRemixxer.css",Method.GET,"src\\main\\frontEnd\\externalRemixxer.css");
+        addRoute("/favicon.ico",Method.GET,"src\\main\\frontEnd\\kaoru.png");
+        addRoute("/remixxerGallery.html",Method.GET,"src\\main\\frontEnd\\remixxerGallery.html");
+        addRoute("/loginRemixxer.html",Method.GET,"src\\main\\frontEnd\\loginRemixxer.html");
+        addRoute("/kaoru.png",Method.GET,"src\\main\\frontEnd\\kaoru.png");
+        addRoute("/what-is-kaworu-sitting-on-v0-hpylib0vwdzd1.webp",Method.GET,"src\\main\\frontEnd\\what-is-kaworu-sitting-on-v0-hpylib0vwdzd1.webp");
     }
     /**
      * @param Takes client path, http code, and server file path
@@ -33,7 +40,13 @@ public class Router {
         try{
             filePath = route.get(path).get(code);
         }catch (NullPointerException e){
-            return new Response(404, ContentType.PLAIN, "File Not Found".getBytes(StandardCharsets.UTF_8));
+            filePath = "src\\main\\frontEnd\\fileNotFound.html";
+            File file = new File(filePath);
+            FileInputStream readFile = new FileInputStream(file);
+            byte[] body = new byte[readFile.available()];
+            readFile.read(body);
+            readFile.close();
+            return new Response(404, ContentType.HTML, body);
         }
         
         System.out.println(filePath);
